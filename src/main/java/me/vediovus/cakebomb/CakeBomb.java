02a39dba1c5aeed6bbb3ef3001126382aae1a480
Aaron Shapiro <vediovus@live.com>
2014-04-05 17:27:16 -0400

    Initial Commit

---
+++ src/main/java/me/vediovus/cakebomb/CakeBomb.java
@@ -0,0 +1,12 @@
+package me.vediovus.cakebomb;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CakeBomb extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
