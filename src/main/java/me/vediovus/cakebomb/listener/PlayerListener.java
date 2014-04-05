d7a324359d99e058f8fbd439994d6d7629999f95
Aaron Shapiro <vediovus@live.com>
2014-04-05 17:48:58 -0400

    Implementation of Cake Bombs

--- src/main/java/me/vediovus/cakebomb/listener/PlayerListener.java
+++ src/main/java/me/vediovus/cakebomb/listener/PlayerListener.java
@@ -1,6 +1,33 @@
 package me.vediovus.cakebomb.listener;
 
+import me.vediovus.cakebomb.bomb.CakeBombConstructor;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PlayerListener implements Listener {
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player p = event.getPlayer();
+        if (event.getClickedBlock().getType().equals(Material.CAKE_BLOCK)) {
+            CakeBombConstructor cake = new CakeBombConstructor(p.getName(), event.getClickedBlock().getLocation());
+            if (cake.isBomb()) {
+                p.getWorld().createExplosion(p.getLocation().getX(),p.getLocation().getY(),p.getLocation().getZ(),1.0F,false,false);
+                p.playSound(p.getLocation(), Sound.NOTE_SNARE_DRUM,5.0F,4.0F);
+                p.sendMessage(ChatColor.RED + "Cake Explodes");
+            } else {
+                if (p.hasPermission("cakebomb.create")) {
+                    if (p.getItemInHand().getType().equals(Material.SULPHUR) && p.getItemInHand().getAmount() == 5) {
+                        p.sendMessage(ChatColor.GREEN + "Your cake has been planted.");
+                        cake.addToList();
+                    }
+                }
+            }
+        }
+    }
 }
