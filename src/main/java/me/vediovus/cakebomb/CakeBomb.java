0adc1bd9987453f8fcf5928b6f56f092d109033e
Aaron Shapiro <vediovus@live.com>
2014-04-05 17:28:29 -0400

    Addition of Listener

--- src/main/java/me/vediovus/cakebomb/CakeBomb.java
+++ src/main/java/me/vediovus/cakebomb/CakeBomb.java
@@ -1,12 +1,14 @@
 package me.vediovus.cakebomb;
 
+import me.vediovus.cakebomb.listener.PlayerListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CakeBomb extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
     }
 
 }
---
+++ src/main/java/me/vediovus/cakebomb/listener/PlayerListener.java
@@ -0,0 +1,6 @@
+package me.vediovus.cakebomb.listener;
+
+import org.bukkit.event.Listener;
+
+public class PlayerListener implements Listener {
+}
